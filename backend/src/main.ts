a079b69aada9765a9e40007ea587bf8ab2996dc0
Matheus <mths.gm@gmail.com>
2024-12-26 23:42:37 -0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { NotFoundInterceptor } from "./interceptors/not-found.interceptor";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // Pipes
   app.useGlobalPipes(
