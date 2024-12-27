dddf8f9712f0af3cf9f41d923278e578afe7c4ed
MatheusGSantos <mths.gm@gmail.com>
2024-12-27 16:06:50 -0300

    feat: add header with title and button for new upload in UploadListPage

--- frontend/src/app/(uploads)/page.tsx
+++ frontend/src/app/(uploads)/page.tsx
@@ -2,6 +2,8 @@ import { fetchUploads } from "@/services/fetchUploads";
 
 import { TableWithPagination } from "@/components/ui/table-with-pagination";
 import { TABLE_COLUMNS } from "./constants";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface PageProps {
   readonly searchParams: {
@@ -20,7 +22,13 @@ export default async function UploadListPage({ searchParams }: PageProps) {
   const { data, meta } = await fetchUploads({ currentPage, perPage });
 
   return (
-    <div className="page">
+    <div className="page gap-8">
+      <header className="flex items-center justify-between">
+        <h1 className="text-4xl font-medium">Uploads</h1>
+        <Button asChild>
+          <Link href="/uploads/new">Novo Upload</Link>
+        </Button>
+      </header>
       <TableWithPagination columns={TABLE_COLUMNS} data={data} meta={meta} />
     </div>
   );
