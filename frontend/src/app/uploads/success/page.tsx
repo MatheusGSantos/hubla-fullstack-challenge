e0ba5e69bf2241c1427e15224743d3178f3ca19f
MatheusGSantos <mths.gm@gmail.com>
2024-12-27 13:50:34 -0300

    refactor: simplify layout classes by introducing a reusable 'page' class for upload components

--- frontend/src/app/uploads/success/page.tsx
+++ frontend/src/app/uploads/success/page.tsx
@@ -3,7 +3,7 @@ import ButtonArea from "./_components/ButtonArea";
 
 export default function UploadSuccess() {
   return (
-    <div className="p-8 flex flex-col w-full h-screen items-center justify-center">
+    <div className="page items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-12">
         <CircleCheckBig size={64} color="#d7ff61" />
         <h1 className="font-medium text-4xl">Seu arquivo foi enviado!</h1>
