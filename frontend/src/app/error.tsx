86fbc38b4cde9c7ececbe598524dffdfd2f8cc43
MatheusGSantos <mths.gm@gmail.com>
2024-12-30 14:03:00 -0300

    feat: add "Go back" button to error page for improved navigation

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -11,7 +11,12 @@ export default function PageError() {
     <div className="page items-center justify-center gap-10">
       <ServerCrash size={64} className="text-red-primary" />
       <h1 className="text-4xl max-w-96 text-center">{DEFAULT_ERROR_MESSAGE}</h1>
-      <Button onClick={() => router.push("/uploads")}>Go to Uploads</Button>
+      <div className="flex gap-4">
+        <Button variant={"outline"} onClick={() => router.back()}>
+          Go back
+        </Button>
+        <Button onClick={() => router.push("/uploads")}>Go to Uploads</Button>
+      </div>
     </div>
   );
 }
